7959c4d3af266e0617094e4f15b6f3344d4ebd06
Daniel Konev <defox4159@gmail.com>
2022-11-08 22:18:04 +0300

    source files changed

---
+++ lab-01_makefile/src/algorithm.c
@@ -0,0 +1,9 @@
+#include "util.h"
+#include "io.h"
+#include "algorithm.h"
+
+int doComputation(int a, double b) {
+  int c = max(a, (int)b);
+  logDebug('d');
+  return c;
+}
---
+++ lab-01_makefile/src/util.c
@@ -0,0 +1,7 @@
+#include "util.h"
+#include "io.h"
+
+int max(int a, int b) {
+    logDebug('m');
+    return a > b ? a : b;
+}
